92d04579e6ec60a352a5d8d5ce279d7326ee9355
DrFloflo <florian.guillot@imt-atlantique.net>
2025-03-29 15:11:43 +0100

    modif webhook

--- src/api/webhook.ts
+++ src/api/webhook.ts
@@ -5,14 +5,14 @@
  * @param create_course - La donnée à transmettre au webhook.
  * @returns La réponse du webhook convertie en JSON.
  */
-async function sendCreateCourse(create_course: string, level: string): Promise<any> {
+async function sendCreateCourse(create_course: string): Promise<any> {
     try {
         const response = await fetch('https://hook.eu1.make.com/ihmym1n82oq1hqb7sa83fa5n5mwa8v48', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ create_course, level })
+            body: JSON.stringify({ create_course })
         });
 
         // Vérification du statut de la réponse
